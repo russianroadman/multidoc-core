aa27df355585ee63374e4339b20e17097079b71a
russianroadman <realgar2@gmail.com>
2021-10-01 14:59:06 +0800

    db

---
+++ api/api.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const cors = require('cors')
+const load = require("./document/builder");
+
+const api = express()
+
+api.use(cors({credentials: true, origin: true}))
+
+api.get('/test', (req, res) => {
+	load('e010c1ce-ed25-4344-8ea2-1369c229ae32').then(r => res.send(r))
+})
+
+/* update document */
+
+
+/* force update document */
+
+
+/* save content */
+
+
+/* save document title */
+
+
+/* save block title */
+
+
+/* save version title */
+
+
+/* check if preferred */
+
+
+/* set as preferred */
+
+
+/* create document */
+
+
+/* add block */
+
+
+/* add version */
+
+
+/* get content */
+
+
+api.post('/post', jp, (req,res) => {
+	console.log(req.body);
+	res.sendStatus(200)
+});
+
+api.listen(port, () => {
+	console.log(`Example app listening at http://localhost:${port}`)
+})
--- server.js
+++ server.js
@@ -1,62 +1,9 @@
 const express = require('express')
 const bp = require('body-parser')
 const cors = require('cors')
-const db = require('./database/database')
-const {Document, Block, Version, Content} = require('./models/multidoc')
 const load = require("./document/builder");
 
 const jp = bp.json()
 const app = express()
 const port = 8080
 
-// db
-//     .sync({force:true})
-//     // .sync()
-//     .then(() => {
-//         return Document.create({
-//             title: 'my first document'
-//         })
-//     })
-//     .then(d => {
-//         return Block.create({
-//             title: 'blockbuster',
-//             documentId: d.id
-//         })
-//     })
-//     .then(b => {
-//         return Version.create({
-//             title: 'Version',
-//             preferred: true,
-//             blockId: b.id
-//         })
-//     })
-//     .then(v => {
-//         return Content.create({
-//             content: '<p>hello world</p>',
-//             versionId: v.id
-//         })
-//     })
-//     .then(() => {
-//         Document.findAll().then(x => {
-//             console.log('all documents: \n')
-//             x.forEach(el => console.log(el))
-//         })
-//     })
-//     .catch(err => console.log(err));
-
-load('e010c1ce-ed25-4344-8ea2-1369c229ae32')
-
-app.use(cors({credentials: true, origin: true}))
-
-app.get('/get', (req, res) => {
-    load('e010c1ce-ed25-4344-8ea2-1369c229ae32').then(r => res.send(r))
-})
-
-app.post('/post', jp, (req,res) => {
-    console.log(req.body);
-    res.sendStatus(200)
-});
-
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
