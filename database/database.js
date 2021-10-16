341e2a1bbe8d1aecacb835e42bbeb2389afc6193
russianroadman <realgar2@gmail.com>
2021-10-16 14:49:10 +0800

    websocket

--- database/database.js
+++ database/database.js
@@ -2,7 +2,8 @@ const Sequelize = require('sequelize')
 
 const db = new Sequelize('multidoc', 'postgres', '1234', {
     dialect: 'postgres',
-    host: 'localhost'
+    host: 'localhost',
+    logging: false
 })
 
 module.exports = db
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const api = require('./api/api')
-const db = require('./database/database')
+const ws = require('websocket').server
+const http = require('http')
 
 const port = 8080
+const server = http.createServer(api);
 
-api.listen(port, () => {
-	console.log(`Example app listening at http://localhost:${port}`)
-})
+server.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
